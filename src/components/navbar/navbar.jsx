c1f04ecb231d5b0ddbd2c44f3bfe97e3b2095f83
Siddharth Makwana <112619536+sidmakwana11@users.noreply.github.com>
2025-07-11 12:21:57 +0530

    Update navbar.jsx

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,47 +1,3 @@
-// import React from 'react';
-// import { FaAngleDown } from 'react-icons/fa';
-// import { Link } from 'react-router-dom';
-
-// import './navbar.css';
-
-// const Navbar = () => {
-//   return (
-//     <nav className="navbar">
-//       <ul className="nav-links">
-//         <li className="dropdown">
-//           Fashion <FaAngleDown className="down-arrow" />
-//           <ul className="dropdown-menu">
-//             <li><Link to="/men">Men</Link></li>
-//             <li><Link to="/women">Women</Link></li>
-//             <li><Link to="/kids">Kids</Link></li>
-//           </ul>
-//         </li>
-//         <li><Link to="/beauty">Beauty</Link></li>
-//         <li className="dropdown">
-//           Sports <FaAngleDown className="down-arrow" />
-//           <ul className="dropdown-menu">
-//             <li><Link to="/sportswear">Sportswear</Link></li>
-//             <li><Link to="/equipments">Equipments</Link></li>
-//           </ul>
-//         </li>
-//         <li className="dropdown">
-//           Electronics <FaAngleDown className="down-arrow" />
-//           <ul className="dropdown-menu">
-//             <li><Link to="/tv">TV</Link></li>
-//             <li><Link to="/mobiles">Mobiles</Link></li>
-//             <li><Link to="/fridge">Fridge</Link></li>
-//             <li><Link to="/headphones">Head/Earphones</Link></li>
-//             <li><Link to="/common">More...</Link></li>
-//           </ul>
-//         </li>
-//         <li><Link to="/furniture">Home & Furniture</Link></li>
-//       </ul>
-//     </nav>
-//   );
-// };
-
-// export default Navbar;
-
 import React, { useEffect, useState } from 'react';
 import { FaAngleDown, FaAngleRight } from 'react-icons/fa';
 import { Link } from 'react-router-dom';
@@ -53,7 +9,7 @@ const Navbar = () => {
   useEffect(() => {
     const fetchHierarchy = async () => {
       try {
-        const res = await fetch("http://localhost:5003/api/categories/hierarchy");
+        const res = await fetch("https://bagit-category-service.onrender.com/api/categories/hierarchy");
         const data = await res.json();
         if (!res.ok) {
           console.error("Hierarchy endpoint failed:", data);
